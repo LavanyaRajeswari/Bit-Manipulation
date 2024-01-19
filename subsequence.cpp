579b9e20c020462677f7777b1d475855cb8fdc08
LavanyaRajeswari <146519974+LavanyaRajeswari@users.noreply.github.com>
2024-01-19 18:08:55 +0530

    Add files via upload

---
+++ subsequence.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fun(vector<int>v,int index,int size,vector<int>ans){
+	if(index >= size){
+		for(int i=0;i<ans.size();i++){
+			cout<<ans[i]<<" ";
+		}
+		cout<<endl;
+		return;
+	}
+	if(ans.size() == 0 || ans.back() < v[index]){
+		ans.push_back(v[index]);
+		fun(v,index+1,size,ans);
+		ans.pop_back();
+		fun(v,index+1,size,ans);
+	}
+	else if(ans.back() > v[index]){
+		fun(v,index+1,size,ans);
+		return;
+	}
+	fun(v,index+1,size,ans);	
+}
+int main(){
+	int n;
+	cin>>n;
+	vector<int>v;
+	for(int i=0;i<n;i++){
+		int data;
+		cin>>data;
+		v.push_back(data);
+	}
+	fun(v,0,v.size(),{});
+}
